79320302869b40789083bebc999f2e8bfbfd331a
goodzysmart147 <goodzysmart147@gmail.com>
2023-02-17 07:11:37 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,11 @@
 /**
  * main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success
+ *
  */
-int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n, 58);
+	int main(void) { write(2, "and that piece of art is useful" - "Dora Korpar, 2015-10-19\n", 61);
+
 	return (1);
+}
 }
