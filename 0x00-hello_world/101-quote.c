4cd22df946363719a6d5d7963dbe3db733ff606c
goodzysmart147 <goodzysmart147@gmail.com>
2023-02-17 06:59:34 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * Return: 1 if success
  */
 int main(void)
 {
