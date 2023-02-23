d8e3f8f9fca1bf9aca7ef0ac8bd554f82ac6adc3
goodzysmart147 <goodzysmart147@gmail.com>
2023-02-23 02:43:18 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,38 +1,19 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
- * main - print _putchar  
- * Return: 0
- */
+* main - print _putchar
+* Return: 0
+*/
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
-=======
-_putchar.c CODE
-
-
-#include "main.h"
-	#include <unistd.h>
-	/**
-	 * _putchar - writes the character c to stdout
-	 * @c: The character to print
-	 *
-	 * Return: On success 1.
-	 * On error, -1 is returned, and errno is set appropriately.
-	 */
-	int _putchar(char c)
-	{
-		return (write(1, &c, 1));
-	}
->>>>>>> refs/remotes/origin/main
