d25eb1295c601c3a0ba9fe4a40a01e116dc3fa63
root <root@GoodyStrings-PC>
2023-10-21 21:37:49 +0100

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,25 +13,28 @@
 
 char *_strdup(char *str)
 {
-	char *newstr;
-	unsigned int i, size = 0;
+	char *ptr, *ret;
+	int size = 0;
 
 	if (!str)
 	{
 		return (NULL);
 	}
 
+	ptr = str;
+	while (*ptr++)
+		size++;
 
-	while (*(str + size++))
-		newstr = malloc(size * sizeof(char));
-	if (newstr == NULL)
+	ret = malloc(size + 1);
+	if (!ret)
 	{
 		return (NULL);
 	}
-	else
-	{
-		for (i = 0; i < size; i++)
-			newstr[i] = str[i];
-	}
-	return (newstr);
+
+	ptr = ret;
+	while (*str)
+		*ptr++ = *str++;
+
+	*ptr = 0;
+	return (ret);
 }
