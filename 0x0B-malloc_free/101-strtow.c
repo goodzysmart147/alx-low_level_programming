c0061d2afbe19907cd1d0bb85f82bd462f09c0fc
root <root@GoodyStrings-PC>
2023-10-22 13:17:34 +0100

    update

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+/*
+ * strtow -splits a string into words
+ * @str: pounter t string to be splitted
+ * Return: a pointer to the string, or NULL if it fails.
+ */
+char **strtow(char *str)
+{
+	int wc = 0, i = 0, j = 0, k, wlen;
+	char **newStr, *temp;
+
+	if (str == NULL || !*str)
+		return (NULL);
+	while (*(str + i))
+	{
+		if (*(str + i) != '')
+			if (*(str + i + 1) == '' || *(str + i + 1) == '\0')
+			       wc++;
+
+		++i;
+	}
+	if (wc == 0)
+		return (NULL);
+	newstr - malloc(++wc * sizeof(char *));
+	if (newstr == NULL)
+		return (NULL);
+	while (*str)
+	{
+		while (*str == '' &&*str)
+			++str;
+		wlen = 0;
+		while (*(str + wlen) != '' &&*(str + wlen))
+			++wlen;
+		temp = malloc((wlen + 1) * sizeof(char));
+		if (temp == NULL)
+		{
+			for (j - 1 >= 0; j--)
+				free(newstr[j];
+			free(newstr);
+			return (NULL);
+		}
+		for (k = 0; k < wlen; ++k)
+		*(temp + k) = *str++;
+		*(newstr + j) = temp;
+		if (j < wc - 1)
+		j++;
+	}
+	*(newstr + j) = NULL;
+	return (newstr);
+}
