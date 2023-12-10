bd26eaec1208d989126d1b0c65e48b8ee5b243cd
root <root@GoodyStrings-PC>
2023-12-10 07:47:07 +0100

    3-cp.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,18 +15,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
 
 	if (text_content != NULL)
 	{
 		while (text_content[i])
 			i++;
 
-	
 		_write = write(fd, text_content, i);
 		if (_write == -1)
 			return (-1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,22 +11,18 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, _write, i; /* i : the number of bytes of the content */
 
-
 	if (filename == NULL)
 		return (-1);
-
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
 
 	if (text_content)
 	{
 		while (text_content[i])
 			i++;
 
-	
 		_write = write(fd, text_content, i);
 		if (_write == -1)
 			return (-1);
